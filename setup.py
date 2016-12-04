8e9b58e3c7ed3a1848c1c5de91d001adc53a4735
JanSiebert <JanSiebert@users.noreply.github.com>
2016-12-04 13:47:47 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'AsyncTelegramBot',
+  packages = ['AsyncTelegramBot'],
+  version = '0.1',
+  description = 'Wrapper for Telegram`s Bot API utilizing asyncio',
+  author = 'Jan Siebert',
+  author_email = 'jansie@live.com',
+  url = 'https://github.com/JanSiebert/async-telegram-bot-python',
+  download_url = 'https://github.com/JanSiebert/async-telegram-bot-python/tarball/0.1',
+  keywords = ['testing', 'async', 'bot', 'api', 'telegram', 'wrapper'],
+  classifiers = [],
+)
